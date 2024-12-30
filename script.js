8d10ec779b33ade567bfc9d751c0a31e6779b367
Jatin-uxx <rawatjatin2001@gmail.com>
2024-12-30 16:43:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+let str=""
+let map= new Map( )
+map.set('M',1000)
+map.set('D', 500)
+map.set('CD', 400)
+map.set('C', 100)
+map.set('XC', 90)
+map.set('L', 50)
+map.set('XL', 40)
+map.set('X', 10)
+map.set('IX', 9)
+map.set('V', 5)
+map.set('IV', 4)
+map.set('I', 1)
+
+      let n=69
+      
+      for(let [char,value] of map)
+      {
+      while(n>=value)
+      {
+           str+=char
+          n-=value
+         
+         
+      }
+      
+      }
+         console.log(str)
+    
+      
+      
 
   //your code here
 
